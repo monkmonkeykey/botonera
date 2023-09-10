f7a9bf401d2d4677cc81c3dd93d85d88274ad310
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2023-09-10 00:53:25 -0600

    Update osc_client.py

--- osc_client.py
+++ osc_client.py
@@ -1,26 +1,16 @@
 from pythonosc import dispatcher, osc_server
 
-# Define funciones para manejar los mensajes OSC
-def manejar_mensaje_1(address, *args):
-    print(f"Recibido mensaje desde {address}: {args}")
-
-def manejar_mensaje_2(address, *args):
-    print(f"Recibido mensaje desde {address}: {args}")
-
-def manejar_mensaje_3(address, *args):
+# Define una función para manejar los mensajes OSC
+def manejar_mensaje(address, *args):
     print(f"Recibido mensaje desde {address}: {args}")
 
 # Crea un despachador de mensajes OSC
 dispatcher = dispatcher.Dispatcher()
-
-# Asocia las direcciones OSC con las funciones de manejo
-dispatcher.map("ch1", manejar_mensaje_1)
-dispatcher.map("ch2", manejar_mensaje_2)
-dispatcher.map("ch3", manejar_mensaje_3)
+dispatcher.map("/ch1", manejar_mensaje)  # Asocia la dirección OSC con la función de manejo
 
 # Configura y corre el servidor OSC
-ip_escucha = "192.168.15.7"  # Escucha en todas las interfaces de red
-puerto_escucha = 9000   # Puerto en el que escucha el servidor
+ip_escucha = "0.0.0.0"  # Escucha en todas las interfaces de red
+puerto_escucha = 8000   # Puerto en el que escucha el servidor
 
 servidor = osc_server.ThreadingOSCUDPServer((ip_escucha, puerto_escucha), dispatcher)
 print(f"Escuchando en {ip_escucha}:{puerto_escucha}")
