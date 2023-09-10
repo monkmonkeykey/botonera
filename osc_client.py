6fbefbd3111cf2783692fbf20e66419ee320d781
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2023-09-10 00:36:20 -0600

    osc

---
+++ osc_client.py
@@ -0,0 +1,18 @@
+from pythonosc import dispatcher, osc_server
+
+# Define una función para manejar los mensajes OSC
+def manejar_mensaje(address, *args):
+    print(f"Recibido mensaje desde {address}: {args}")
+
+# Crea un despachador de mensajes OSC
+dispatcher = dispatcher.Dispatcher()
+dispatcher.map("/mi_mensaje", manejar_mensaje)  # Asocia la dirección OSC con la función de manejo
+
+# Configura y corre el servidor OSC
+ip_escucha = "0.0.0.0"  # Escucha en todas las interfaces de red
+puerto_escucha = 8000   # Puerto en el que escucha el servidor
+
+servidor = osc_server.ThreadingOSCUDPServer((ip_escucha, puerto_escucha), dispatcher)
+print(f"Escuchando en {ip_escucha}:{puerto_escucha}")
+
+servidor.serve_forever()
