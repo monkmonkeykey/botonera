23bf47d8f58416c3413671756b1a94b779b6d37e
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2023-09-10 09:49:45 -0600

    Update osc_client.py

--- osc_client.py
+++ osc_client.py
@@ -1,23 +1,39 @@
 import threading
 from pythonosc import dispatcher, osc_server
+import RPi.GPIO as GPIO
 
-# Define funciones para manejar los mensajes OSC
+# Configurar pines GPIO para los LEDs
+GPIO.setmode(GPIO.BCM)
+LED_PIN_1 = 23  # Cambia el número de pin según tu configuración
+LED_PIN_2 = 22  # Cambia el número de pin según tu configuración
+LED_PIN_3 = 13  # Cambia el número de pin según tu configuración
+GPIO.setup(LED_PIN_1, GPIO.OUT)
+GPIO.setup(LED_PIN_2, GPIO.OUT)
+GPIO.setup(LED_PIN_3, GPIO.OUT)
+
+# Define funciones para manejar los mensajes OSC y controlar los LEDs
 def manejar_mensaje_1(address, *args):
     print(f"Recibido mensaje desde {address}: {args}")
+    if args[0] == 1:
+        GPIO.output(LED_PIN_1, GPIO.HIGH)
+    else:
+        GPIO.output(LED_PIN_1, GPIO.LOW)
 
 def manejar_mensaje_2(address, *args):
     print(f"Recibido mensaje desde {address}: {args}")
+    if args[0] == 1:
+        GPIO.output(LED_PIN_2, GPIO.HIGH)
+    else:
+        GPIO.output(LED_PIN_2, GPIO.LOW)
 
 def manejar_mensaje_3(address, *args):
     print(f"Recibido mensaje desde {address}: {args}")
+    if args[0] == 1:
+        GPIO.output(LED_PIN_3, GPIO.HIGH)
+    else:
+        GPIO.output(LED_PIN_3, GPIO.LOW)
 
-# Crea un despachador de mensajes OSC
-dispatcher = dispatcher.Dispatcher()
-
-# Asocia las direcciones OSC con las funciones de manejo
-dispatcher.map("/ch1", manejar_mensaje_1)
-dispatcher.map("/ch2", manejar_mensaje_2)
-dispatcher.map("/ch3", manejar_mensaje_3)
+# Resto del código sin cambios
 
 # Configura y corre el servidor OSC en hilos separados
 ip_escucha = "0.0.0.0"  # Escucha en todas las interfaces de red
