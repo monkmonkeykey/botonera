fa7d877ea054e10853c065e4fe366586401d1a5a
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2023-09-10 13:47:02 -0600

    Update lectura_botones.py

--- lectura_botones.py
+++ lectura_botones.py
@@ -1,16 +1,26 @@
 import board
 import digitalio
 
-# Configura el pin del botón como una entrada digital
-pin_del_boton = digitalio.DigitalInOut(board.D17)  # Cambia D2 al número de pin que estés usando
-pin_del_boton.direction = digitalio.Direction.INPUT
-pin_del_boton.pull = digitalio.Pull.DOWN  # Opcional: configura la resistencia pull-down si es necesario
+# Configura los pines de los botones como entradas digitales
+pines_de_botones = [
+    digitalio.DigitalInOut(board.D17),
+    digitalio.DigitalInOut(board.D18),
+    digitalio.DigitalInOut(board.D24),
+    digitalio.DigitalInOut(board.D25),
+    digitalio.DigitalInOut(board.D5),
+    digitalio.DigitalInOut(board.D6),
+    digitalio.DigitalInOut(board.D16),
+    digitalio.DigitalInOut(board.D19),
+]
+
+# Configura la dirección de los pines como entrada y, opcionalmente, la resistencia pull-down
+for pin in pines_de_botones:
+    pin.direction = digitalio.Direction.INPUT
+    pin.pull = digitalio.Pull.DOWN  # Opcional: configura la resistencia pull-down si es necesario
 
 while True:
-    # Lee el estado del botón
-    estado_del_boton = pin_del_boton.value
+    # Lee el estado de cada botón y almacénalo en una lista
+    estados_de_botones = [pin.value for pin in pines_de_botones]
 
-    if estado_del_boton:
-        print("El botón está presionado")
-    else:
-        print("El botón no está presionado")
+    # Imprime los estados de los botones
+    print("Estados de los botones:", estados_de_botones)
