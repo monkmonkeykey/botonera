80d4d60039294e55ad8502f02a841a07b2da188e
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2023-09-10 09:51:56 -0600

    Update osc_client.py

--- osc_client.py
+++ osc_client.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 LED_PIN_1 = 23  # Cambia el número de pin según tu configuración
 LED_PIN_2 = 22  # Cambia el número de pin según tu configuración
-LED_PIN_3 = 13  # Cambia el número de pin según tu configuración
+LED_PIN_3 = 26  # Cambia el número de pin según tu configuración
 GPIO.setup(LED_PIN_1, GPIO.OUT)
 GPIO.setup(LED_PIN_2, GPIO.OUT)
 GPIO.setup(LED_PIN_3, GPIO.OUT)
@@ -34,6 +34,14 @@ def manejar_mensaje_3(address, *args):
         GPIO.output(LED_PIN_3, GPIO.LOW)
 
 # Resto del código sin cambios
+
+# Crea un despachador de mensajes OSC
+dispatcher = dispatcher.Dispatcher()
+
+# Asocia las direcciones OSC con las funciones de manejo
+dispatcher.map("/ch1", manejar_mensaje_1)
+dispatcher.map("/ch2", manejar_mensaje_2)
+dispatcher.map("/ch3", manejar_mensaje_3)
 
 # Configura y corre el servidor OSC en hilos separados
 ip_escucha = "0.0.0.0"  # Escucha en todas las interfaces de red
