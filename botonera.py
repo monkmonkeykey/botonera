69be9851919ab0bf4df79d01319a823eaf878dcc
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2023-09-09 23:02:49 -0600

    Update botonera.py

--- botonera.py
+++ botonera.py
@@ -4,10 +4,10 @@ import RPi.GPIO as GPIO
 import time
 
 # Configura el cliente OSC
-client = SimpleUDPClient("192.168.15.7", 1000)  # Cambia la dirección y el puerto según tus necesidades
+client = SimpleUDPClient("192.168.15.7", 10000)  # Cambia la dirección y el puerto según tus necesidades
 
 # Configura los pines GPIO de los botones
-PINES_BOTONES = [17, 18, 24, 25 ,5, 6, 16, 19]
+PINES_BOTONES = [17, 18, 24, 25, 5, 6, 16, 19]
 
 GPIO.setmode(GPIO.BCM)
 
@@ -18,18 +18,22 @@ for pin in PINES_BOTONES:
 def enviar_mensaje_osc(address, *args):
     client.send_message(address, args)
 
-# Bucle para leer el estado de los botones y enviar mensajes OSC
+# Variables para el seguimiento del estado anterior de los botones
+estado_anterior = [1] * len(PINES_BOTONES)
+
+# Bucle para leer el estado de los botones y enviar mensajes OSC cuando haya cambios
 try:
     while True:
         for i, pin in enumerate(PINES_BOTONES):
             estado_boton = GPIO.input(pin)
-            direccion_osc = f"/boton{i + 1}"
 
-            # Envía mensajes OSC en función del estado del botón
-            if estado_boton == 0:
-                enviar_mensaje_osc(direccion_osc, 1)  # Botón presionado
-            else:
-                enviar_mensaje_osc(direccion_osc, 0)  # Botón liberado
+            # Verifica si ha habido un cambio en el estado del botón
+            if estado_boton != estado_anterior[i]:
+                direccion_osc = f"/boton{i + 1}"
+                estado_anterior[i] = estado_boton
+
+                # Envía un mensaje OSC con el estado actual del botón
+                enviar_mensaje_osc(direccion_osc, estado_boton)
 
         time.sleep(0.1)  # Pequeña pausa para evitar lecturas repetidas
 
