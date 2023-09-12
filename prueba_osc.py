6b10c224f1a504c5ef64c6044fbecafa9dd07515
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2023-09-12 00:28:28 -0600

    Update prueba_osc.py

--- prueba_osc.py
+++ prueba_osc.py
@@ -1,10 +1,29 @@
 import threading
 from pythonosc import dispatcher, osc_server
+import board
+import busio
+import digitalio
+
+import adafruit_tlc5947
+
+SCK = board.SCK
+MOSI = board.MOSI
+LATCH = digitalio.DigitalInOut(board.D27)
+
+# Initialize SPI bus.
+spi = busio.SPI(clock=SCK, MOSI=MOSI)
+
+# Initialize TLC5947
+tlc5947 = adafruit_tlc5947.TLC5947(spi, LATCH)
+
+led1 = tlc5947.create_pwm_out(0)
+led2 = tlc5947.create_pwm_out(1)
+led3 = tlc5947.create_pwm_out(2)
 
 # Define funciones para manejar los mensajes OSC
 def manejar_mensaje_1(address, *args):
     print(f"Recibido mensaje desde {address}: {args}")
-
+    led1.duty_cycle = {args}
 def manejar_mensaje_2(address, *args):
     print(f"Recibido mensaje desde {address}: {args}")
 
