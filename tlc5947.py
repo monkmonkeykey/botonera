5474e50611d9b46b10df4a053a96bda28e8bfaad
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2023-09-13 00:33:28 -0600

    Update tlc5947.py

--- tlc5947.py
+++ tlc5947.py
@@ -50,13 +50,13 @@ while True:
 
 
 # Incremento de 0 a 10
-    for i in range(0, 49151 ,100):
+    for i in range(0, 49151 ,250):
         intensity_level = i
         green.duty_cycle = intensity_level
         print("Nivel de intensidad:", intensity_level)
 
     # Decremento de 10 a 0
-    for i in range(49150, -1, -100):
+    for i in range(49150, -1, -250):
         intensity_level = i
         green.duty_cycle = intensity_level
         print("Nivel de intensidad:", intensity_level)
