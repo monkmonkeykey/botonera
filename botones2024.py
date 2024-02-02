d8a0947fa5add97b284ffea118fb1c2c20b329d6
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2024-02-02 00:13:18 -0600

    Create botones2024.py

---
+++ botones2024.py
@@ -0,0 +1,72 @@
+import threading
+from pythonosc import dispatcher, osc_server
+from pythonosc.udp_client import SimpleUDPClient
+import time
+import board
+import neopixel
+from gpiozero import Button
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+# Configuración del servidor OSC
+ip_escucha = "0.0.0.0"
+puerto_escucha = 8000
+dispatcher = dispatcher.Dispatcher()
+direcciones_osc = ["/ch1", "/ch2", "/ch3"]
+for direccion in direcciones_osc:
+    dispatcher.map(direccion, manejar_led)
+
+# Configuración del cliente OSC
+cliente_osc = SimpleUDPClient("192.168.15.8", 10000)  # Cambia la dirección y el puerto según tus necesidades
+
+# Configuración de hardware SPI y MCP3008
+SPI_PORT = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+# Configuración de botones y LEDs
+BOTONES = [13, 26, 27, 21]
+buttons = [Button(pin, pull_up=True) for pin in BOTONES]
+pixels = neopixel.NeoPixel(board.D18, 8)
+
+# Variables para el seguimiento del estado anterior de los botones
+estado_anterior = [True] * len(BOTONES)
+
+# Función para enviar un mensaje OSC
+def enviar_mensaje_osc(address, *args):
+    cliente_osc.send_message(address, args)
+
+# Función para controlar los LEDs
+def controlar_leds():
+    while True:
+        # Tu lógica para controlar los LEDs aquí
+        # Por ejemplo, puedes actualizar los valores de duty_cycle de los LEDs aquí
+        time.sleep(0.01)  # Asegúrate de agregar un pequeño retraso para evitar que el hilo consuma demasiada CPU
+
+# Bucle para leer el estado de los botones y enviar mensajes OSC cuando haya cambios
+def leer_botones():
+    try:
+        while True:
+            for i, button in enumerate(buttons):
+                estado_boton = button.is_pressed
+                # Verifica si ha habido un cambio en el estado del botón
+                if estado_boton != estado_anterior[i]:
+                    direccion_osc = f"/boton{i + 1}"
+                    estado_anterior[i] = estado_boton
+                    # Envía un mensaje OSC con el estado actual del botón
+                    enviar_mensaje_osc(direccion_osc, int(estado_boton))
+            enviar_mensaje_osc("/pot", int(mcp.read_adc(0)))
+            time.sleep(0.01)  # Pequeña pausa para evitar lecturas repetidas
+
+    except KeyboardInterrupt:
+        pass
+
+# Inicia los hilos
+servidor_thread = threading.Thread(target=servidor.serve_forever)
+servidor_thread.start()
+
+leds_thread = threading.Thread(target=controlar_leds)
+leds_thread.start()
+
+botones_thread = threading.Thread(target=leer_botones)
+botones_thread.start()
