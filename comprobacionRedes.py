5f7a13cb46eaa0ff980201da1f5ebe2ab8dd21b1
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2024-02-02 01:55:34 -0600

    Update comprobacionRedes.py

--- comprobacionRedes.py
+++ comprobacionRedes.py
@@ -1,26 +1,19 @@
-import os
+import subprocess
 import time
 
-def is_spi_active():
-    spi_path = "/sys/class/spi_master/"
-    return any(os.path.isdir(os.path.join(spi_path, d)) for d in os.listdir(spi_path))
-
+programa_a_ejecutar = "/home/pi/PowerRoom/botonera/botones2024.py"
 def check_ssh():
     try:
         subprocess.run(["systemctl", "is-active", "ssh"], check=True)
+        subprocess.run(["python", programa_a_ejecutar])
         return True
     except subprocess.CalledProcessError:
         return False
 
-# Verificar si el servicio SPI está activo
-if is_spi_active():
-    print("El servicio SPI está activo en la Raspberry Pi.")
+# Intentar comprobar hasta que el servicio SSH esté activo
+while not check_ssh():
+    print("Esperando que el servicio SSH se active...")
+    time.sleep(5)  # Espera 5 segundos antes de volver a verificar
 
-    # Intentar comprobar hasta que el servicio SSH esté activo
-    while not check_ssh():
-        print("Esperando que el servicio SSH se active...")
-        time.sleep(5)  # Espera 5 segundos antes de volver a verificar
+print("El servicio SSH está activo. Continuando con el programa.")
 
-    print("El servicio SSH está activo. Continuando con el programa.")
-else:
-    print("El servicio SPI no está activo en la Raspberry Pi.")
