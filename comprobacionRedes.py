17266e39426e3cac8f553cbd05a4b334698c74c1
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2024-02-02 01:36:59 -0600

    Update comprobacionRedes.py

--- comprobacionRedes.py
+++ comprobacionRedes.py
@@ -16,30 +16,3 @@ while not check_ssh():
 
 print("El servicio SSH está activo. Continuando con el programa.")
 
-
-
-
-import subprocess
-
-
-
-
-def check_ssh():
-    try:
-        subprocess.run(["systemctl", "is-active", "ssh"], check=True)
-        print("El servicio SSH está activo.")
-        # Ejecuta el programa
-        
-    except subprocess.CalledProcessError:
-        print("El servicio SSH no está activo.")
-
-def check_ping(host="8.8.8.8"):
-    try:
-        subprocess.run(["ping", "-c", "1", host], check=True)
-        print(f"Se pudo hacer ping a {host}.")
-    except subprocess.CalledProcessError:
-        print(f"No se pudo hacer ping a {host}.")
-
-# Ejemplo de uso
-check_ssh()
-check_ping()
