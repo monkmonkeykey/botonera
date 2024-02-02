6a2d59643f38d670be7eff32b302c354e4bf7925
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2024-02-02 01:03:15 -0600

    Update botones2024.py

--- botones2024.py
+++ botones2024.py
@@ -9,7 +9,7 @@ from gpiozero import Button
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
-
+time.sleep(30)
 SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
