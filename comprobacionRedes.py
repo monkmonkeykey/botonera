af9fd39468db0ed26a364c2b09424c3d2b3ecf41
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2024-02-02 01:47:36 -0600

    Update comprobacionRedes.py

--- comprobacionRedes.py
+++ comprobacionRedes.py
@@ -1,18 +1,34 @@
 import subprocess
 import time
+
+import subprocess
+import time
 programa_a_ejecutar = "/home/pi/PowerRoom/botonera/botones2024.py"
+def is_spi_enabled():
+    try:
+        with open('/boot/config.txt', 'r') as config_file:
+            config_content = config_file.read()
+            return 'dtparam=spi=on' in config_content
+    except FileNotFoundError:
+        return False
+
 def check_ssh():
     try:
         subprocess.run(["systemctl", "is-active", "ssh"], check=True)
-        subprocess.run(["python", programa_a_ejecutar])
         return True
     except subprocess.CalledProcessError:
         return False
 
-# Intentar comprobar hasta que el servicio SSH esté activo
-while not check_ssh():
-    print("Esperando que el servicio SSH se active...")
-    time.sleep(5)  # Espera 5 segundos antes de volver a verificar
+# Verificar si SPI está habilitado
+if is_spi_enabled():
+    print("SPI está habilitado en la Raspberry Pi.")
+    subprocess.run(["python", programa_a_ejecutar])
+    # Intentar comprobar hasta que el servicio SSH esté activo
+    while not check_ssh():
+        print("Esperando que el servicio SSH se active...")
+        time.sleep(5)  # Espera 5 segundos antes de volver a verificar
 
-print("El servicio SSH está activo. Continuando con el programa.")
+    print("El servicio SSH está activo. Continuando con el programa.")
+else:
+    print("SPI no está habilitado en la Raspberry Pi.")
 
