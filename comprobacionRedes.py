1bc29a711274b82dbc326ff1164cff3630a4a85d
monkmonkeykeycentro <123058166+monkmonkeykeycentro@users.noreply.github.com>
2024-02-02 01:34:52 -0600

    Update comprobacionRedes.py

--- comprobacionRedes.py
+++ comprobacionRedes.py
@@ -1,24 +1,16 @@
 import subprocess
-programa_a_ejecutar = "/home/pi/PowerRoom/botonera2024.py"
-
-
+import time
 
 def check_ssh():
     try:
         subprocess.run(["systemctl", "is-active", "ssh"], check=True)
-        print("El servicio SSH está activo.")
-        # Ejecuta el programa
-        subprocess.run(["python", programa_a_ejecutar])
+        return True
     except subprocess.CalledProcessError:
-        print("El servicio SSH no está activo.")
+        return False
 
-def check_ping(host="8.8.8.8"):
-    try:
-        subprocess.run(["ping", "-c", "1", host], check=True)
-        print(f"Se pudo hacer ping a {host}.")
-    except subprocess.CalledProcessError:
-        print(f"No se pudo hacer ping a {host}.")
+# Intentar comprobar hasta que el servicio SSH esté activo
+while not check_ssh():
+    print("Esperando que el servicio SSH se active...")
+    time.sleep(2)  # Espera 5 segundos antes de volver a verificar
 
-# Ejemplo de uso
-check_ssh()
-check_ping()
+print("El servicio SSH está activo. Continuando con el programa.")
