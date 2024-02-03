06449c308b013b8327194dd5b3a10853f7da4381
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2024-02-02 18:37:53 -0600

    Update lectura_pot.py

--- lectura_pot.py
+++ lectura_pot.py
@@ -1,5 +1,5 @@
-# Simple example of reading the MCP3008 analog input channels using its
-# differential mode.  Will print the difference of channel 0 and 1.
+# Simple example of reading the MCP3008 analog input channels and printing
+# them all out.
 # Author: Tony DiCola
 # License: Public Domain
 import time
@@ -10,29 +10,30 @@ import Adafruit_MCP3008
 
 
 # Software SPI configuration:
-# CLK  = 18
-# MISO = 23
-# MOSI = 24
-# CS   = 25
-# mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+#CLK  = 18
+#MISO = 23
+#MOSI = 24
+#CS   = 25
+#mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 # Hardware SPI configuration:
 SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
-print('Press Ctrl-C to quit...')
+
+print('Reading MCP3008 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+print('-' * 57)
+# Main program loop.
 while True:
-    # Grab the difference between channel 0 and 1 (i.e. channel 0 minus 1).
-    # Note you can specify any value in 0-7 to grab other differences:
-    #  - 0: Return channel 0 minus channel 1
-    #  - 1: Return channel 1 minus channel 0
-    #  - 2: Return channel 2 minus channel 3
-    #  - 3: Return channel 3 minus channel 2
-    #  - 4: Return channel 4 minus channel 5
-    #  - 5: Return channel 5 minus channel 4
-    #  - 6: Return channel 6 minus channel 7
-    #  - 7: Return channel 7 minus channel 6
-    value = mcp.read_adc_difference(0)
-    print('Channel 0 minus 1: {0}'.format(value))
+    # Read all the ADC channel values in a list.
+    values = [0]*8
+    #for i in range(8):
+        # The read_adc function will get the value of the specified channel (0-7).
+    values[0] = mcp.read_adc(0)
+    # Print the ADC values.
+    print('| {0:>4}'.format(*values))
+    # Pause for half a second.
     time.sleep(0.5)
