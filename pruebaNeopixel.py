130a7f52298f0f70b0ac77f101f375f64c3f9ee1
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2024-02-16 17:21:06 -0600

    Update pruebaNeopixel.py

--- pruebaNeopixel.py
+++ pruebaNeopixel.py
@@ -1,24 +1,26 @@
 import time
 import board
 import neopixel
-numPixels = 8
-# Definir el objeto NeoPixel
-pixels = neopixel.NeoPixel(board.D18, numPixels, brightness=1.0, auto_write=False)
 
-def set_pixel_color(pixel, color):
-    pixels[pixel] = color
-    pixels.show()
+# Definir el número de Neopixels en el módulo
+num_pixels = 8
 
+# Definir el pin al que está conectado el módulo
+pin = board.D18
+
+# Crear el objeto NeoPixel para el módulo completo
+pixels = neopixel.NeoPixel(pin, num_pixels, brightness=1.0, auto_write=False)
+
+# Bucle principal
 while True:
-    # Definir un color (por ejemplo, rojo)
-    color = (255, 0, 0)
-
-    # Establecer el color del píxel 0
-    set_pixel_color(0, color)
-
-    # Variar el brillo del color
+    # Definir colores para cada LED de forma independiente
+    for i in range(num_pixels):
+        color = ((i * 30) % 256, (i * 50) % 256, (i * 70) % 256)
+        pixels[i] = color
+    
+    # Variar el brillo de la cadena completa
     for b in range(256):
-        pixels.brightness = b / 255.0
+        pixels.brightness = int(b) / 255.0
         pixels.show()
         time.sleep(0.01)
 
